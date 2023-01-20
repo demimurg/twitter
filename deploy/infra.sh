a046701ee3294e09e8858163e7e1d2585ce00004
demimurg <blohinalexej@mail.ru>
2023-01-21 01:08:41 +0300

    deploy: describe all infra in helm chart

--- deploy/infra.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# grafana
-helm install grafana grafana-labs/grafana --set persistence.enabled=true --set service.type=NodePort --set service.nodePort=30002
-# loki, promtail
-helm install loki grafana-labs/loki-stack
