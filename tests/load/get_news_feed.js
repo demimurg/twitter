b967fc3d520ce955c8981820c191afd43d8f6ea8
demimurg <blohinalexej@mail.ru>
2023-05-04 18:34:27 +0300

    test: add k6 base loading test

---
+++ tests/load/get_news_feed.js
@@ -0,0 +1,16 @@
+import grpc from 'k6/net/grpc';
+import {check} from 'k6';
+
+const client = new grpc.Client();
+client.load(['../../pkg/proto', '.'], 'twitter.proto');
+
+export default () => {
+    client.connect('twitter.demimurg.com:30000', {plaintext: true});
+
+    const data = {user_id: 1, limit: 50};
+    const response = client.invoke('Twitter/GetNewsFeed', data);
+    check(response, {
+        'status is OK': (r) => r && r.status === grpc.StatusOK,
+    });
+    client.close();
+};
